a1ca752e9258e3d4c74aa6ecf370869a867c9779
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-16 09:58:05 -0300

    Herança em C#

---
+++ 04-POO/ExemploPOO/Models/Professor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    class Professor : Pessoa
+    {
+        public double Salario { get; set; }
+        public int Documento { get; set; }
+    }
+}
--- 04-POO/ExemploPOO/Program.cs
+++ 04-POO/ExemploPOO/Program.cs
@@ -6,11 +6,19 @@ namespace ExemploPOO
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa();
-
+            Aluno p1 = new Aluno();
+            
             p1.Nome = "Bob";
             p1.Idade = 20;
+            p1.Nota = 9;
             p1.Apresentar();
+
+            Professor p2 = new Professor();
+            
+            p2.Nome = "Jonh";
+            p2.Idade = 42;
+            p2.Documento = 123456;
+            p2.Apresentar();
         }
     }
 }
