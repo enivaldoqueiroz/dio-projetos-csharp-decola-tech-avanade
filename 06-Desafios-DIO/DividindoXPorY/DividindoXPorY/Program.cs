faa90cf1c0a6b0e959bd22297b8ecc2a932b8c5a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-01 20:02:43 -0300

    Desafio C# -  2 / 5 - Dividindo X por Y

---
+++ 06-Desafios-DIO/DividindoXPorY/DividindoXPorY/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace DividindoXPorY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desafio C# -  2 / 5 - Dividindo X por Y
+
+            int limit = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+                if (Y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+
+                }else if (X == 0)
+                {
+                    double divisao = 0;
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    double divisao = X / Y;
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
