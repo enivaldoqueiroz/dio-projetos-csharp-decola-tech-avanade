a0e22bf483d380f72fed9003bba508b2c4068bad
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-01 20:38:19 -0300

    Desafio C# - 5 / 5 - Pedro Bento e o Mundo de OZ

---
+++ 06-Desafios-DIO/PedroBentoEOMundoDeOZ/PedroBentoEOMundoDeOZ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PedroBentoEOMundoDeOZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desafio C# - 5 / 5 - Pedro Bento e o Mundo de OZ
+            var J = new List<string>();
+            while (true)
+            {
+                string joia = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(joia))
+                    J.Add(joia);
+                else
+                {
+                    break;
+                }
+            }
+            var njoias = J.Distinct().ToList();
+            Console.WriteLine(njoias.Count);
+        }
+    }
+}
