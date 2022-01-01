3de8fe51dcbc46d7186a6518d3bf2816e1301834
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-01 19:59:42 -0300

    Desafio C# - 1 / 5 - Ho Ho Ho

---
+++ 06-Desafios-DIO/HoHoHo/HoHoHo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HoHoHo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desafio C# - 1 / 5 - Ho Ho Ho
+
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i <= N; i++)
+            {
+                if (i < (N - 1))
+                {
+                    Console.Write("Ho ");                  //Complete o código no espaço em branco
+                }
+                else if (i == N)
+                {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
