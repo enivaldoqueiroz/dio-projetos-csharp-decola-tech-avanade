aa08e8d8fbc61c3d807f3d80c1a9ac5dc1da9ac5
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-07 19:51:25 -0300

    Ordenação via bubble sort atraves do metodo OrdernarBubblerSort()

---
+++ 09-FundamentosDeColecaoesELINQComDotNet/09-FundamentosDeColecaoesELINQComDotNet/Helper/OperacoesArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_FundamentosDeColecaoesELINQComDotNet.Helper
+{
+    class OperacoesArray
+    {
+        public void OrdernarBubblerSort(ref int[] array)
+        {
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public void ImprimirArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
--- 09-FundamentosDeColecaoesELINQComDotNet/09-FundamentosDeColecaoesELINQComDotNet/Program.cs
+++ 09-FundamentosDeColecaoesELINQComDotNet/09-FundamentosDeColecaoesELINQComDotNet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _09_FundamentosDeColecaoesELINQComDotNet.Helper;
+using System;
 
 namespace _09_FundamentosDeColecaoesELINQComDotNet
 {
@@ -6,6 +7,37 @@ namespace _09_FundamentosDeColecaoesELINQComDotNet
     {
         static void Main(string[] args)
         {
+
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] { 6, 3, 8, 1, 9};
+
+            Console.WriteLine("Array Original: ");
+            op.ImprimirArray(array);
+            op.OrdernarBubblerSort(ref array);
+
+            Console.WriteLine("Array Ordenado: ");
+            op.ImprimirArray(array);
+
+            /*
+
+            // ******** - Operações com matriz multidimensional, sendo composta de 4 linhas e 2 colunas - ********
+            int[,] matriz = new int[4, 2]
+            {
+                { 8, 8 },
+                { 10, 20 },
+                { 50, 100 },
+                { 90, 200 }
+            };
+
+            // Percorrendo a matriz
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    System.Console.WriteLine(matriz[i, j]);
+                }
+            }
+
             // ******** - Operações com Array - ********
             int[] arrayInteiros = new int[3];
 
@@ -27,25 +59,7 @@ namespace _09_FundamentosDeColecaoesELINQComDotNet
             foreach (int item in arrayInteiros)
             {
                 System.Console.WriteLine(item);
-            }
-
-            // ******** - Operações com matriz multidimensional, sendo composta de 4 linhas e 2 colunas - ********
-            int[,] matriz = new int[4, 2]
-            {
-                { 8, 8 },
-                { 10, 20 },
-                { 50, 100 },
-                { 90, 200 }
-            };
-
-            // Percorrendo a matriz
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    System.Console.WriteLine(matriz[i, j]);
-                }
-            }
+            }*/
         }
     }
 }
