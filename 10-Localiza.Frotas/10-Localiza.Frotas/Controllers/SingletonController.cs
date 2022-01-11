c8aef8f80283112125d7771ff8b2398d597e4c38
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-11 20:47:15 -0300

    Criando o projeto e ambiente com Swagger

---
+++ 10-Localiza.Frotas/10-Localiza.Frotas/Controllers/SingletonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _10_Localiza.Frotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SingletonController : ControllerBase
+    {
+    }
+}
