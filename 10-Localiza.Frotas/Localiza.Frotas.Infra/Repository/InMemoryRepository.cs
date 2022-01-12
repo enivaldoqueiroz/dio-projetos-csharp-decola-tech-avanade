c4eea261077eb8c67f6e7ef572f85de5fd288900
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-12 00:23:24 -0300

    Simulando banco dedados com InMemoryRepository

---
+++ 10-Localiza.Frotas/Localiza.Frotas.Infra/Repository/InMemoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using Localiza.Frotas.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Localiza.Frotas.Infra.Repository
+{
+    class InMemoryRepository : IVeiculoRepository
+    {
+        public void Add(Veiculo veiculo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Veiculo veiculo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object veiculo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Veiculo> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Veiculo GetVeiculo(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Veiculo veiculo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
