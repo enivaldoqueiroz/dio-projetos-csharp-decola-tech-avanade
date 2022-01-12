0d750b4c9e2ababb754557e155ca4dd7d5a54354
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-12 00:10:52 -0300

    Entendendo o padrão Repository

---
+++ 10-Localiza.Frotas/10-Localiza.Frotas/Controllers/VeiculosController.cs
@@ -0,0 +1,60 @@
+﻿using Localiza.Frotas.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _10_Localiza.Frotas.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly IVeiculoRepository veiculoRepository;
+        public VeiculosController(IVeiculoRepository veiculoRepository)
+        {
+            this.veiculoRepository = veiculoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get() => Ok(veiculoRepository.GetAll());
+
+        [HttpGet]
+        public IActionResult Get(Guid id)
+        {
+            var veiculo = veiculoRepository.GetById(id);
+            if (veiculo == null)
+                return NotFound();
+            return Ok(veiculo);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Veiculo veiculo)
+        {
+            veiculoRepository.Add(veiculo);
+            return CreatedAtAction(nameof(Get), new { id = veiculo.Id }, veiculo);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] Veiculo veiculo)
+        {
+            veiculoRepository.Update(veiculo);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var veiculo = veiculoRepository.GetById(id);
+            if (veiculo == null)
+            {
+                return NoContent();
+            }
+
+            veiculoRepository.Delete(veiculo);
+            return NoContent();
+        }
+    }
+}
---
+++ 10-Localiza.Frotas/Localiza.Frotas.Domain/IVeiculoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Localiza.Frotas.Domain
+{
+    public interface IVeiculoRepository
+    {
+        Veiculo GetVeiculo(Guid id);
+
+        IEnumerable<Veiculo> GetAll();
+
+        void Add(Veiculo veiculo);
+        void Delete(Veiculo veiculo);
+        void Update(Veiculo veiculo);
+        object GetById(Guid id);
+        void Delete(object veiculo);
+    }
+}
