3cf1e93f25a8d081bb03104f51755d13e9dfc288
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-18 00:35:25 -0300

    2Especificidades do comando Select

--- 11-SQLServer/sql-server-criando-as-primeiras-consultas.sql
+++ 11-SQLServer/sql-server-criando-as-primeiras-consultas.sql
@@ -129,3 +129,34 @@ select *
 from Clientes
 where Codigo = 1 
 OR TipoPessoa = 'J'
+
+
+select * from Clientes
+
+select * from Produtos
+
+insert Produtos values(1, 'Caneca', 'Caneca Azul', 1.5)
+insert Produtos values(2, 'Caderno', 'Caneca 10 Materia', 20.99)
+
+select * from Pedido
+
+insert Pedido values(1, GETDATE(), 0, 3, 7)
+insert Pedido values(2, GETDATE(), 0, 22.49, 1)
+
+select * from Pedido
+select * from PedidoItem
+
+insert PedidoItem values(1, 1, 1.5, 2)
+insert PedidoItem values(2, 1, 1.5, 1)
+insert PedidoItem values(2, 2, 20.99, 1)
+
+select *,
+		case 
+			when TipoPessoa = 'J' then 'Juridica'
+			when TipoPessoa = 'F' then 'Fisica'
+			else 'Pessoa indefinidada'
+		end
+from Clientes
+
+select *, CONVERT(varchar, DataSolicitacao, 103)
+from Pedido
