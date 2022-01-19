ab0a5566bfe088f936dfb8eb80127543649eb78a
enivaldo queiroz <enivaldo.jecq@gmail.com>
2022-01-19 11:37:24 -0300

    Comando Create Table e criação da primeira tabela Planetas

--- 11-SQLServer/sql-server-modelando-um-banco-de-dados-na-pratica-com-sql-server.sql
+++ 11-SQLServer/sql-server-modelando-um-banco-de-dados-na-pratica-com-sql-server.sql
@@ -1,4 +1,25 @@
 -- Modelando um banco de dados Star Wars na prática com SQL SERVER
 
 --API
-https://www.swapi.dev/api
+https://swapi.dev/api
+https://swapi.dev/
+
+--Comando Create Table e criação da primeira tabela Planetas
+
+CREATE TABLE Planetas
+(
+	IdPlaneta	INT			NOT NULL,
+	Nome		VARCHAR(50) NOT NULL,
+	Rotacao		FLOAT		NOT NULL,
+	Orbita		FLOAT		NOT NULL,
+	Diametro	FLOAT		NOT NULL,
+	Clima		VARCHAR(50) NOT NULL,
+	Populacao	INT			NOT NULL
+)
+
+GO
+ALTER TABLE Planetas 
+	ADD CONSTRAINT PK_Planetas 
+	PRIMARY KEY (IdPlaneta);
+
+
