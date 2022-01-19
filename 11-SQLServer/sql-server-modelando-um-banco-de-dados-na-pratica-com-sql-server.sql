01b91c745127e2e2b760620299fe039ea0d1ee66
enivaldo queiroz <enivaldo.jecq@gmail.com>
2022-01-19 11:51:45 -0300

    Criação da tabela Naves e a Pilotos

--- 11-SQLServer/sql-server-modelando-um-banco-de-dados-na-pratica-com-sql-server.sql
+++ 11-SQLServer/sql-server-modelando-um-banco-de-dados-na-pratica-com-sql-server.sql
@@ -22,4 +22,30 @@ ALTER TABLE Planetas
 	ADD CONSTRAINT PK_Planetas 
 	PRIMARY KEY (IdPlaneta);
 
+-- Criação da tabela Naves e a Pilotos
 
+CREATE TABLE Naves
+(
+	IdNave		INT				NOT NULL,
+	Nome		VARCHAR(100)	NOT NULL,
+	Passageiros INT				NOT NULL,
+	Carga		FLOAT			NOT NULL,
+	Classe		VARCHAR(100)	NOT NULL
+)
+GO
+ALTER TABLE Naves 
+	ADD CONSTRAINT PK_Naves 
+	PRIMARY KEY (IdNave);
+
+CREATE TABLE Pilotos
+(
+	IdPiloto INT NOT NULL,
+	Nome VARCHAR(200) NOT NULL,
+	AnoNascimento VARCHAR(20) NOT NULL,
+	IdPlaneta INT NOT NULL
+)
+GO
+ALTER TABLE Pilotos ADD CONSTRAINT PK_Pilotos PRIMARY KEY (IdPiloto)
+GO
+ALTER TABLE Pilotos ADD CONSTRAINT FK_Pilotos_Planetas FOREIGN KEY (IdPlaneta)
+REFERENCES Planetas (IdPlaneta)
